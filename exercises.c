cbf026539a1fa415f5ee2a555b30ffe20e0cedb5
Your Name <you@example.com>
2024-03-13 21:02:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,8 +45,6 @@ ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]) {
-  int size3 = size1 + size2;
-  
   for (int i = 0; i< size1 ; i++)
       result[i] = arr1[i];
 
